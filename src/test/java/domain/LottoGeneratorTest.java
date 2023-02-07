e9b9f372022ad9a58fb24bda195778bb17c72601
Junek3 <rlarudrms28@gmail.com>
2023-02-08 02:09:03 +0900

    refactor: static 메소드 변경에 따른 LottoGeneratorTest 수정

--- src/test/java/domain/LottoGeneratorTest.java
+++ src/test/java/domain/LottoGeneratorTest.java
@@ -1,29 +1,18 @@
 package domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class LottoGeneratorTest {
-
-    private LottoGenerator generator = new LottoGenerator();
-
-    @BeforeEach
-    void createLottoGenerator() {
-        generator = new LottoGenerator();
-    }
 
     @Test
     @DisplayName("로또생성기 생성")
     void createLotto() {
         // when
-        List<Integer> lottoNumbers = generator.generate();
+        List<Integer> lottoNumbers = LottoGenerator.generate();
         // then
         Assertions.assertThat(lottoNumbers.size()).isEqualTo(6);
     }
