8c3527c26b132a8b4504d72ef07e4b4669ffcdb4
euichanhwang <pine6971@naver.com>
2023-02-08 22:14:52 +0900

    test : 당첨 번호는 숫자이며 여섯개임을 확인하는 테스트 코드 작성

--- src/test/java/convertor/LottoNumberConvertorTest.java
+++ src/test/java/convertor/LottoNumberConvertorTest.java
@@ -1,10 +1,14 @@
 package convertor;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.Arrays;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import domain.Numbers;
 
@@ -19,7 +23,22 @@ public class LottoNumberConvertorTest {
 		List<Integer> winningNumbers = numbers.getNumbers();
 		List<Integer> expectedNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-		Assertions.assertThat(winningNumbers).isEqualTo(expectedNumbers);
+		assertThat(winningNumbers).isEqualTo(expectedNumbers);
+	}
+
+	@Test
+	void 당첨_번호는_숫자여야_한다() {
+		assertThatThrownBy(() -> LottoNumberConvertor.convertWinningNumber("a"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("[ERROR] 당첨 번호는 숫자여야 합니다. 다시 입력해주세요");
+	}
+
+	@ParameterizedTest(name = "{index} : 당첨 번호 {0} 입력")
+	@ValueSource(strings = {"1, 2, 3, 4, 5", "1, 2, 3, 4, 5, 6, 7"})
+	void 당첨_번호는_여섯개여야_한다(String input) {
+		assertThatThrownBy(() -> LottoNumberConvertor.convertWinningNumber(input))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("[ERROR] 당첨 번호는 여섯개여야 합니다.");
 	}
 
 }
