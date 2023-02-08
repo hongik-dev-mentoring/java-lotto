628028b2e2aad15ef1d9c0e9ee767a2b8d1569eb
euichanhwang <pine6971@naver.com>
2023-02-08 17:13:56 +0900

    feat : Integer List 일급 콜렉션 Numbers 구현

---
+++ src/main/java/domain/Numbers.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class Numbers {
+	private final List<Integer> numbers;
+
+	public Numbers(List<Integer> numbers) {
+		this.numbers = numbers;
+	}
+
+	public List<Integer> getNumbers() {
+		return List.copyOf(numbers);
+	}
+}
