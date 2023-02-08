f9fc0afe2b0120eff5a926f0834a9900a5e3267f
Junek3 <rlarudrms28@gmail.com>
2023-02-09 01:31:00 +0900

    refactor: LottoGenerator의 generate 메소드가 LottoDto를 반환하도록 변경

--- src/test/java/domain/LottoGeneratorTest.java
+++ src/test/java/domain/LottoGeneratorTest.java
@@ -4,16 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 class LottoGeneratorTest {
 
     @Test
     @DisplayName("로또생성기 생성")
     void createLotto() {
         // when
-        List<Integer> lottoNumbers = LottoGenerator.generate();
+        LottoDto lottoDto = LottoGenerator.generate();
         // then
-        Assertions.assertThat(lottoNumbers.size()).isEqualTo(6);
+        Assertions.assertThat(lottoDto.getLottoNumbers().size()).isEqualTo(6);
     }
 }
