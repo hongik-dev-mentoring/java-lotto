336719cfeddbbe137021a1142eb9428027d4e02b
euichanhwang <pine6971@naver.com>
2023-02-08 21:42:11 +0900

    feat : 사용자에게 로또 당첨 번호를 받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class InputView {
 
 	private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+	private static final String INPUT_WINNING_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 	private static final Scanner console = new Scanner(System.in);
 
 	public static String getPurchaseAmount() {
 		System.out.println(INPUT_MONEY_MESSAGE);
+		return console.nextLine();
+	}
+
+	public static String getWinningNumbers() {
+		System.out.println(INPUT_WINNING_LOTTO_NUMBER_MESSAGE);
 		return console.nextLine();
 	}
 }
