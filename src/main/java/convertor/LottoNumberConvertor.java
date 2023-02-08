9338e979a23b9702b52023f4c1e02d1673f91530
euichanhwang <pine6971@naver.com>
2023-02-08 22:15:24 +0900

    feat : 당첨 번호는 숫자이며 여섯개임을 확인하는 코드 구현

--- src/main/java/convertor/LottoNumberConvertor.java
+++ src/main/java/convertor/LottoNumberConvertor.java
@@ -8,13 +8,26 @@ import domain.Numbers;
 
 public class LottoNumberConvertor {
 	private static final String DELIMITER = ",";
+	private static final int LOTTO_NUMBER = 6;
 
 	public static Numbers convertWinningNumber(String winningNumber) {
-		String[] split = winningNumber.split(DELIMITER);
-		List<Integer> winnerNumbers = Arrays.stream(split)
-			.map(String::trim)
-			.map(Integer::parseInt)
-			.collect(Collectors.toList());
-		return new Numbers(winnerNumbers);
+		try {
+			String[] split = winningNumber.split(DELIMITER);
+			List<Integer> winnerNumbers = Arrays.stream(split)
+				.map(String::trim)
+				.map(Integer::parseInt)
+				.collect(Collectors.toList());
+
+			validateWinningNumber(winnerNumbers);
+			return new Numbers(winnerNumbers);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다. 다시 입력해주세요");
+		}
+	}
+
+	private static void validateWinningNumber(List<Integer> winnerNumbers) {
+		if (winnerNumbers.size() != LOTTO_NUMBER) {
+			throw new IllegalArgumentException("[ERROR] 당첨 번호는 여섯개여야 합니다.");
+		}
 	}
 }
