cfc79e8b01c5ee66baa5098d2ab9542d41baec4d
Junek3 <rlarudrms28@gmail.com>
2023-02-09 01:34:50 +0900

    feat: ResultView 구현하기

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,7 +1,9 @@
 package view;
 
 import domain.LottoDto;
+import domain.LottoEnum;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ResultView {
@@ -14,5 +16,23 @@ public class ResultView {
                 .collect(Collectors.joining(", "));
         System.out.print(result);
         System.out.println("]");
+    }
+
+    public static void printStatistics(Map<String, Integer> lottoStatistics) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        for (Map.Entry<String, Integer> lottoEntry : lottoStatistics.entrySet()) {
+            LottoEnum lottoEnum = LottoEnum.valueOf(lottoEntry.getKey());
+            System.out.println(lottoEnum.getPrizeText() + "- " + lottoEntry.getValue() + "개");
+        }
+    }
+
+    public static void printBenefit(Integer inputPrice, Map<String, Integer> statisticsMap) {
+        int rewardSum = 0;
+        for (Map.Entry<String, Integer> lottoEntry : statisticsMap.entrySet()) {
+            LottoEnum lottoEnum = LottoEnum.valueOf(lottoEntry.getKey());
+            rewardSum += lottoEnum.getReward() * lottoEntry.getValue();
+        }
+        System.out.printf("%.2f", (double) rewardSum / inputPrice);
     }
 }
