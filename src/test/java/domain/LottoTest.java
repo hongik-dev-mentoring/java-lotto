da5a963d7f1842d2bff13bc44f208c6316ce24d6
euichanhwang <pine6971@naver.com>
2023-02-08 21:03:21 +0900

    chore : 변수와 상수 컨벤션에 맞게 변경

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -13,6 +13,7 @@ class LottoTest {
 	private static final int END_NUMBER = 7;
 	private static final int PICK_NUMBERS = 6;
 	private static final int LOTTO_NUMBERS = 10;
+	private static final int BONUS_NUMBER = 7;
 
 	@Test
 	void 임의_갯수의_로또_번호를_가진_로또_객체를_만들_수_있다() {
@@ -25,10 +26,10 @@ class LottoTest {
 	void 로또_번호를_입력하면_당첨_결과를_받을_수_있다() {
 		RandomNumberPicker randomNumberPicker = new RandomNumberPicker(START_NUMBER, END_NUMBER, PICK_NUMBERS);
 		Lotto lotto = Lotto.generateLottoWithLottoNumbers(randomNumberPicker, LOTTO_NUMBERS);
-		Numbers numbers = new Numbers(Arrays.asList(1, 2, 3, 4, 5, 6));
-		Integer bonusBall = 7;
+		Numbers pickNumbers = new Numbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+		Integer bonusBall = BONUS_NUMBER;
 
-		EnumMap<Ranking, Integer> lottoRanking = lotto.checkLottoResult(numbers, bonusBall);
+		EnumMap<Ranking, Integer> lottoRanking = lotto.checkLottoResult(pickNumbers, bonusBall);
 		Integer actualWinnerNumber = lottoRanking.get(Ranking.FIRST);
 
 		assertThat(actualWinnerNumber).isEqualTo(LOTTO_NUMBERS);
