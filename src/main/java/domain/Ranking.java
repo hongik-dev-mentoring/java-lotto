a1f9d7d2ef12e8aad0a95f8483e3a0d833c3af6b
euichanhwang <pine6971@naver.com>
2023-02-08 21:02:29 +0900

    feat : Ranking이 당첨 번호 일치 갯수에 따라 Ranking을 반환하는 기능 구현

--- src/main/java/domain/Ranking.java
+++ src/main/java/domain/Ranking.java
@@ -1,5 +1,25 @@
 package domain;
 
 public enum Ranking {
-	FIRST, SECOND, THIRD, FOURTH, FIFTH;
+
+	FIRST, SECOND, THIRD, FOURTH, FIFTH, UNRANKED;
+
+	public static Ranking getRanking(int correctNumber) {
+		if (correctNumber < 3) {
+			return UNRANKED;
+		}
+
+		if (correctNumber == 3) {
+			return FIFTH;
+		}
+
+		if (correctNumber == 4) {
+			return FOURTH;
+		}
+
+		if (correctNumber == 6) {
+			return FIRST;
+		}
+		return THIRD;
+	}
 }
