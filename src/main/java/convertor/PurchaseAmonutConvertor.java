4825ccd39a310b75f14aa1511a5d5d3067e86a10
euichanhwang <pine6971@naver.com>
2023-02-08 21:34:49 +0900

    feat : 로또 구입 금액을 Integer로 변환하는 기능 구현

---
+++ src/main/java/convertor/PurchaseAmonutConvertor.java
@@ -0,0 +1,12 @@
+package convertor;
+
+public class PurchaseAmonutConvertor {
+
+	public static Integer getPurchaseAmount(String purchaseAmountMoney) {
+		try {
+			return Integer.parseInt(purchaseAmountMoney);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 로또 금액은 숫자여야 합니다. 다시 입력해주세요");
+		}
+	}
+}
