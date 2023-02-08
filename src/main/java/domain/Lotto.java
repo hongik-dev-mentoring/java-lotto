052ebbc9d6465481a5a2550739fdae42949db437
euichanhwang <pine6971@naver.com>
2023-02-08 18:15:31 +0900

    feat : 로또 객체 구현

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+
+	private static final int START_NUMBER = 1;
+	private static final int END_NUMBER = 46;
+	private static final int PICK_NUMBERS = 6;
+	private final LottoNumbers lottoNumbers;
+	private final RandomNumberPicker randomNumberPicker;
+
+	private Lotto(int numberOfLotto) {
+		this.randomNumberPicker = new RandomNumberPicker(START_NUMBER, END_NUMBER, PICK_NUMBERS);
+		this.lottoNumbers = generateLottoNumbers(numberOfLotto);
+	}
+
+	public static Lotto generateLottoWithLottoNumbers(int numOfLotto) {
+		return new Lotto(numOfLotto);
+	}
+
+	private LottoNumbers generateLottoNumbers(int numberOfLotto) {
+		List<Numbers> numbersList = new ArrayList<>();
+		for (int i = 0; i < numberOfLotto; i++) {
+			numbersList.add(randomNumberPicker.pickNumber());
+		}
+		return new LottoNumbers(numbersList);
+	}
+
+	public LottoNumbers getLottoNumbers() {
+		return lottoNumbers;
+	}
+}
