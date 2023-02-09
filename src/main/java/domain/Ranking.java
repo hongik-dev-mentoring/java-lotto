7ee53567eea9f609c23e12e2fd188e1c65883d14
euichanhwang <pine6971@naver.com>
2023-02-09 16:49:19 +0900

    feat : 랭킹에 따른 상금을 Ranking enum 객체가 가지고 있도록 구현

--- src/main/java/domain/Ranking.java
+++ src/main/java/domain/Ranking.java
@@ -2,7 +2,18 @@ package domain;
 
 public enum Ranking {
 
-	FIRST, SECOND, THIRD, FOURTH, FIFTH, UNRANKED;
+	FIRST(2000000000), SECOND(30000000), THIRD(1500000),
+	FOURTH(50000), FIFTH(5000), UNRANKED(0);
+
+	private int winningAmount;
+
+	Ranking(int winningAmount) {
+		this.winningAmount = winningAmount;
+	}
+
+	public int getWinningAmount() {
+		return winningAmount;
+	}
 
 	public static Ranking getRanking(int correctNumber) {
 		if (correctNumber < 3) {
