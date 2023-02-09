aa7e57dec10eb18fdb2e9edad961cab3621be443
euichanhwang <pine6971@naver.com>
2023-02-09 15:47:51 +0900

    refactor : LottoNumberConvertor의 검증 책임을 LottoNumbers 일급 컬렉션에게 위임

--- src/main/java/util/convertor/LottoNumberConvertor.java
+++ src/main/java/util/convertor/LottoNumberConvertor.java
@@ -7,8 +7,8 @@ import java.util.stream.Collectors;
 import domain.LottoNumbers;
 
 public class LottoNumberConvertor {
+
 	private static final String DELIMITER = ",";
-	private static final int LOTTO_NUMBER = 6;
 
 	public static LottoNumbers convertWinningNumber(String winningNumber) {
 		try {
@@ -18,21 +18,15 @@ public class LottoNumberConvertor {
 				.map(Integer::parseInt)
 				.collect(Collectors.toList());
 
-			validateWinningNumber(winnerNumbers);
 			return new LottoNumbers(winnerNumbers);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다. 다시 입력해주세요");
 		}
 	}
 
-	private static void validateWinningNumber(List<Integer> winnerNumbers) {
-		if (winnerNumbers.size() != LOTTO_NUMBER) {
-			throw new IllegalArgumentException("[ERROR] 당첨 번호는 여섯개여야 합니다.");
-		}
-	}
-
 	public static Integer convertBonusNumber(String bonusNumber) {
 		try {
+
 			return Integer.parseInt(bonusNumber);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException("[ERROR] 보너스 번호는 한 개의 숫자여야 합니다. 다시 입력해주세요");
