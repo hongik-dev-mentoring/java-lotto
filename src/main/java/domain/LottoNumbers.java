5b816be6b53b7c284c004d09c6ad79fe7348e3ab
euichanhwang <pine6971@naver.com>
2023-02-09 14:46:40 +0900

    feat : LottoNumbers 의 숫자가 여섯 개인지 확인하는 코드 작성

--- src/main/java/domain/LottoNumbers.java
+++ src/main/java/domain/LottoNumbers.java
@@ -4,9 +4,17 @@ import java.util.List;
 
 public class LottoNumbers {
 	private final List<Integer> numbers;
+	private static final int LOTTO_NUMBER = 6;
 
 	public LottoNumbers(List<Integer> numbers) {
 		this.numbers = numbers;
+		validateLottoNumbersLength(numbers);
+	}
+
+	private void validateLottoNumbersLength(List<Integer> numbers) {
+		if (numbers.size() != LOTTO_NUMBER) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 여섯 개의 숫자여야 합니다.");
+		}
 	}
 
 	public List<Integer> getNumbers() {
@@ -16,4 +24,6 @@ public class LottoNumbers {
 	public boolean contains(Integer value) {
 		return numbers.contains(value);
 	}
+
+
 }
