51d07b0763407aae36577875cab8326450ecb8c1
Junek3 <rlarudrms28@gmail.com>
2023-02-10 03:05:47 +0900

    refactor: LottoController의 startLotto 메소드 분리하기

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -3,6 +3,7 @@ package controller;
 import domain.LottoChecker;
 import domain.LottoDto;
 import domain.LottoGenerator;
+import domain.LottoWinningNumbers;
 import view.Input;
 import view.ResultView;
 
@@ -18,18 +19,30 @@ public class LottoController {
         Integer purchaseNum = inputPrice/1000;
         System.out.println(purchaseNum + "개를 구매했습니다.");
 
+        List<LottoDto> lottoDtoList = generateLottoDtoList(purchaseNum);
+        LottoWinningNumbers lottoWinningNumbers = readWinningNumbers();
+        createLottoStatistics(inputPrice, lottoDtoList, lottoWinningNumbers);
+    }
+
+    private List<LottoDto> generateLottoDtoList(int purchaseNum) {
         List<LottoDto> lottoDtoList = new ArrayList<>();
         for (int i = 0; i < purchaseNum; i++) {
             LottoDto lottoDto = LottoGenerator.generate();
             lottoDtoList.add(lottoDto);
             ResultView.printLottoNumbers(lottoDto);
         }
+        return lottoDtoList;
+    }
+
+    private LottoWinningNumbers readWinningNumbers() {
+        Input input = new Input();
         List<Integer> lastLottoNumbers = input.readLastLottoNumbers();
+        int bonusNumber = input.readBonusNumber();
+        return new LottoWinningNumbers(lastLottoNumbers, bonusNumber);
+    }
 
-        Integer bonusNumber = input.readBonusNumber();
-
-        LottoChecker lottoChecker = new LottoChecker(lottoDtoList,lastLottoNumbers ,bonusNumber);
-
+    private void createLottoStatistics(Integer inputPrice, List<LottoDto> lottoDtoList, LottoWinningNumbers lottoWinningNumbers) {
+        LottoChecker lottoChecker = new LottoChecker(lottoDtoList, lottoWinningNumbers);
         Map<String, Integer> statisticsMap = lottoChecker.calculateLottoStatistics();
         ResultView.printStatistics(statisticsMap);
         ResultView.printBenefit(inputPrice, statisticsMap);
