57fabcd505bc68d73701cda124aef9e259ee05bb
euichanhwang <pine6971@naver.com>
2023-02-10 10:19:28 +0900

    feat : 로또 금액이 0원 이상인지 검증하는 메서드 구현

--- src/main/java/util/convertor/PurchaseAmountConvertor.java
+++ src/main/java/util/convertor/PurchaseAmountConvertor.java
@@ -4,9 +4,17 @@ public class PurchaseAmountConvertor {
 
 	public static Integer convertPurchaseAmount(String purchaseAmountMoney) {
 		try {
-			return Integer.parseInt(purchaseAmountMoney);
+			Integer purchaseAmount = Integer.parseInt(purchaseAmountMoney);
+			validatePositive(purchaseAmount);
+			return purchaseAmount;
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException("[ERROR] 로또 금액은 숫자여야 합니다. 다시 입력해주세요");
+		}
+	}
+
+	private static void validatePositive(Integer amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("[ERROR] 로또 금액은 0원 이상이어야 합니다. 다시 입력해주세요");
 		}
 	}
 }
