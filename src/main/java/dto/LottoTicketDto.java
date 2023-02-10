a27866c9a643d1ddcd8303c4c4365bcae082f63a
euichanhwang <pine6971@naver.com>
2023-02-10 09:56:21 +0900

    feat : 구매한 로또 번호를 전달하는 Dto 객체 구현

---
+++ src/main/java/dto/LottoTicketDto.java
@@ -0,0 +1,22 @@
+package dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import domain.LottoTicket;
+
+public class LottoTicketDto {
+
+	List<LottoNumbersDto> lottoNumbers;
+
+	public LottoTicketDto(LottoTicket lottoTicket) {
+		this.lottoNumbers = lottoTicket.getLottoTicket()
+			.stream()
+			.map(LottoNumbersDto::new)
+			.collect(Collectors.toList());
+	}
+
+	public List<LottoNumbersDto> getLottoNumbersDto() {
+		return List.copyOf(lottoNumbers);
+	}
+}
