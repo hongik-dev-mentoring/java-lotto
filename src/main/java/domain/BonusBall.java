3a4dd40ceef670fc74b4a81e4a5f6d8de942baf4
euichanhwang <pine6971@naver.com>
2023-02-10 11:22:00 +0900

    feat : 보너스 볼과 당첨 번호의 중복을 검사하는 메서드 구현

--- src/main/java/domain/BonusBall.java
+++ src/main/java/domain/BonusBall.java
@@ -7,8 +7,9 @@ public class BonusBall {
 		this.bonusBallNumber = bonusBallNumber;
 	}
 
-	public static BonusBall createBonusBallInRange(int startNumber, int endNumber, int bonusBallNumber) {
+	public static BonusBall createBonusBallInRange(int startNumber, int endNumber, int bonusBallNumber, WinningNumbers winningNumbers) {
 		validateBonusBallNumber(startNumber, endNumber, bonusBallNumber);
+		validateDuplicateWithWinningNumber(bonusBallNumber, winningNumbers);
 		return new BonusBall(bonusBallNumber);
 	}
 
@@ -16,6 +17,16 @@ public class BonusBall {
 		if (bonusBallNumber >= endNumber || bonusBallNumber < startNumber) {
 			throw new IllegalArgumentException("[ERROR] 보너스 볼은 1부터 45까지의 숫자여야 합니다.");
 		}
+	}
+
+	private static void validateDuplicateWithWinningNumber(int bonusBallNumber, WinningNumbers winningNumbers) {
+		if (isContains(bonusBallNumber, winningNumbers)) {
+			throw new IllegalArgumentException("[ERROR] 보너스 볼은 당첨 번호와 중복이 불가능합니다. ");
+		}
+	}
+
+	private static boolean isContains(int bonusBallNumber, WinningNumbers winningNumbers) {
+		return winningNumbers.contains(bonusBallNumber);
 	}
 
 	public int getBonusBallNumber() {
