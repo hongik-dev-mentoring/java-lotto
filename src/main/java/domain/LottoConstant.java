a8ed8b041c1dd37f84cb60cd319791b103bf49d5
LTSGOD <lts0424@naver.com>
2023-02-13 02:13:22 +0900

    refactor: class에서 Enum으로 변경

--- src/main/java/domain/LottoConstant.java
+++ src/main/java/domain/LottoConstant.java
@@ -1,10 +1,20 @@
 package domain;
 
-public class LottoConstant {
-    public static final String FIRST_PRIZE_KEY = "PRIZE_1ST";
-    public static final String SECOND_PRIZE_KEY = "PRIZE_2ND";
-    public static final String THIRD_PRIZE_KEY = "PRIZE_3RD";
-    public static final String FOURTH_PRIZE_KEY = "PRIZE_4TH";
-    public static final String FIFTH_PRIZE_KEY = "PRIZE_5TH";
-    public static final String NO_PRIZE_KEY = "NO_PRIZE";
+public enum LottoConstant {
+    FIRST_PRIZE_KEY("PRIZE_1ST"),
+    SECOND_PRIZE_KEY("PRIZE_2ND"),
+    THIRD_PRIZE_KEY("PRIZE_3RD"),
+    FOURTH_PRIZE_KEY("PRIZE_4TH"),
+    FIFTH_PRIZE_KEY("PRIZE_5TH"),
+    NO_PRIZE_KEY("NO_PRIZE");
+
+    private final String prizeName;
+
+    LottoConstant(String prizeName) {
+        this.prizeName = prizeName;
+    }
+
+    public String getPrizeName() {
+        return prizeName;
+    }
 }
