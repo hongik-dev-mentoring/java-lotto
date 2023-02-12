bf2c5914ef6f5f49cff1312735b47fb530b6bd6e
LTSGOD <lts0424@naver.com>
2023-02-13 02:12:05 +0900

    refactor: 3항 함수 제거
    - LOTTO_PRICE domain으로 위치 변경

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,9 +1,6 @@
 package controller;
 
-import domain.LottoChecker;
-import domain.LottoDto;
-import domain.LottoGenerator;
-import domain.LottoWinningNumbers;
+import domain.*;
 import view.Input;
 import view.ResultView;
 
@@ -12,40 +9,31 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoController {
-    private static final int LOTTO_PRICE = 1000;
 
     public void startLotto() {
-        Input input = new Input();
-        int inputPrice = input.readPrice();
-        int purchaseNum = inputPrice / LOTTO_PRICE;
+        int inputPrice = Input.readPrice();
+        int purchaseNum = inputPrice / LottoChecker.getLottoPrice();
         ResultView.printPurchaseInfo(purchaseNum);
 
-        List<LottoDto> lottoDtoList = generateLottoDtoList(purchaseNum);
-        LottoWinningNumbers lottoWinningNumbers = readWinningNumbers();
-        createLottoStatistics(inputPrice, lottoDtoList, lottoWinningNumbers);
-    }
-
-    private List<LottoDto> generateLottoDtoList(int purchaseNum) {
-        List<LottoDto> lottoDtoList = new ArrayList<>();
+        List<LottoNumbers> lottoNumbersGroup = new ArrayList<>();
         for (int i = 0; i < purchaseNum; i++) {
-            LottoDto lottoDto = LottoGenerator.generate();
-            lottoDtoList.add(lottoDto);
-            ResultView.printLottoNumbers(lottoDto);
+            LottoNumbers lottoNumbers = LottoGenerator.generate();
+            lottoNumbersGroup.add(lottoNumbers);
+            ResultView.printLottoNumbers(lottoNumbers);
         }
-        return lottoDtoList;
+
+        ResultView.printInputGuide();
+        LottoWinningNumbers lottoWinningNumbers = readWinningNumbers();
+        LottoChecker lottoChecker = new LottoChecker(lottoNumbersGroup, lottoWinningNumbers);
+
+        Map<String, Integer> statisticsMap = lottoChecker.calculateLottoStatistics();
+        ResultView.printStatistics(statisticsMap);
+        ResultView.printBenefit(lottoChecker.getBenefit(statisticsMap));
     }
 
     private LottoWinningNumbers readWinningNumbers() {
-        Input input = new Input();
-        List<Integer> lastLottoNumbers = input.readLastLottoNumbers();
-        int bonusNumber = input.readBonusNumber();
+        List<Integer> lastLottoNumbers = Input.readLastLottoNumbers();
+        int bonusNumber = Input.readBonusNumber();
         return new LottoWinningNumbers(lastLottoNumbers, bonusNumber);
-    }
-
-    private void createLottoStatistics(Integer inputPrice, List<LottoDto> lottoDtoList, LottoWinningNumbers lottoWinningNumbers) {
-        LottoChecker lottoChecker = new LottoChecker(lottoDtoList, lottoWinningNumbers);
-        Map<String, Integer> statisticsMap = lottoChecker.calculateLottoStatistics();
-        ResultView.printStatistics(statisticsMap);
-        ResultView.printBenefit(inputPrice, statisticsMap);
     }
 }
