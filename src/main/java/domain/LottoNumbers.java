9203fea1adb1184d0126002932af4b020b542285
euichanhwang <pine6971@naver.com>
2023-02-13 23:06:18 +0900

    refactor : 예외 메시지를 상수로 변경한다
    로또 번호의 개수가 변경될 경우를 고려한다

--- src/main/java/domain/LottoNumbers.java
+++ src/main/java/domain/LottoNumbers.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class LottoNumbers implements Numbers {
 
+	private static final String LOTTO_NUMBER_COUNT_MESSAGE = "[ERROR] 로또 번호는 여섯 개의 숫자여야 합니다.";
+	private static final String LOTTO_NUMBER_DUPLICATE_MESSAGE = "[ERROR] 로또 번호는 중복이 불가능합니다.";
 	private static final int LOTTO_NUMBER = 6;
 
 	private final List<Integer> numbers;
@@ -16,14 +18,14 @@ public class LottoNumbers implements Numbers {
 
 	private void validateLottoNumbersLength(List<Integer> numbers) {
 		if (numbers.size() != LOTTO_NUMBER) {
-			throw new IllegalArgumentException("[ERROR] 로또 번호는 여섯 개의 숫자여야 합니다.");
+			throw new IllegalArgumentException(LOTTO_NUMBER_COUNT_MESSAGE);
 		}
 	}
 
 	private void validateDuplicatedNumber(List<Integer> numbers) {
 		long count = getDistinctLottoNumber(numbers);
 		if (count != numbers.size()) {
-			throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 불가능합니다.");
+			throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE_MESSAGE);
 		}
 	}
 
