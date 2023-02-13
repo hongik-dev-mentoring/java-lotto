140e89aff8df14bded49ae34f28ad4e5c9fde3c4
euichanhwang <pine6971@naver.com>
2023-02-13 20:48:42 +0900

    chore : 불필요한 개행을 제거한다

--- src/test/java/util/convertor/PurchaseAmountConvertorTest.java
+++ src/test/java/util/convertor/PurchaseAmountConvertorTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 class PurchaseAmountConvertorTest {
 
 	@Test
-	void 구입_금액을_숫자로_변횐할수_있다() {
+	void 구입_금액을_숫자로_변환할수_있다() {
 		Integer purchaseAmount = PurchaseAmountConvertor.convertPurchaseAmount("14000");
 
 		assertThat(purchaseAmount).isEqualTo(14000);
@@ -18,7 +18,6 @@ class PurchaseAmountConvertorTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"a", "ABC"})
 	void 로또_구입_금액은_숫자여야_한다(String input) {
-
 		assertThatThrownBy(() -> PurchaseAmountConvertor.convertPurchaseAmount(input))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("[ERROR] 로또 금액은 숫자여야 합니다. 다시 입력해주세요");
@@ -26,7 +25,6 @@ class PurchaseAmountConvertorTest {
 
 	@Test
 	void 로또_구입_금액은_0원_이상이어야_한다() {
-
 		assertThatThrownBy(() -> PurchaseAmountConvertor.convertPurchaseAmount("-22000"))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("[ERROR] 로또 금액은 0원 이상이어야 합니다. 다시 입력해주세요");
