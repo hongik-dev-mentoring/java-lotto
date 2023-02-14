8912ffd0f5806d905c16e968d8646f5b466e057c
LTSGOD <lts0424@naver.com>
2023-02-14 14:31:07 +0900

    refactor: Lotto한장 가격 클래스로 분리

--- src/main/java/domain/LottoPrice.java
+++ src/main/java/domain/LottoPrice.java
@@ -1,2 +1,9 @@
-package domain;public class LottoPrice {
+package domain;
+
+public class LottoPrice {
+    private static final int LOTTO_PRICE = 1000;
+
+    public static int getLottoPrice() {
+        return LOTTO_PRICE;
+    }
 }
