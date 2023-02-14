bc68c5398d0342de9158b8d4a9ae9f0df13ea947
Junek3 <rlarudrms28@gmail.com>
2023-02-14 10:56:03 +0900

    refactor: 리팩토링에 맞게 테스트코드 수정하기

--- src/test/java/domain/LottoNumberCounterTest.java
+++ src/test/java/domain/LottoNumberCounterTest.java
@@ -6,33 +6,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoNumberCounterTest {
-
     private LottoNumberCounter lottoNumberCounter;
+    private LottoWinningNumbers lottoWinningNumbers;
 
     @BeforeEach
     void setUpTest() {
         LastLottoNumbers lastLottoNumbers = new LastLottoNumbers("1,2,3,4,5,6");
         BonusNumber bonusNumber = new BonusNumber("7");
-        LottoWinningNumbers lottoWinningNumbers = new LottoWinningNumbers(lastLottoNumbers, bonusNumber);
-        lottoNumberCounter = new LottoNumberCounter(lottoWinningNumbers);
+        lottoWinningNumbers = new LottoWinningNumbers(lastLottoNumbers, bonusNumber);
+        lottoNumberCounter = new LottoNumberCounter();
     }
 
     @Test
     @DisplayName("당첨번호 카운트 테스트")
     void countLottoNumbersTest() {
-        lottoNumberCounter.countLottoNumbers(1);
-        lottoNumberCounter.countLottoNumbers(2);
-        lottoNumberCounter.countLottoNumbers(3);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 1);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 2);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 3);
         Assertions.assertThat(lottoNumberCounter.getCount()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("보너스번호 카운트 테스트")
     void countBonusNumberTest() {
-        lottoNumberCounter.countLottoNumbers(1);
-        lottoNumberCounter.countLottoNumbers(2);
-        lottoNumberCounter.countLottoNumbers(3);
-        lottoNumberCounter.countBonusNumber(7);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 1);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 2);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 3);
+        lottoNumberCounter.countBonusNumber(lottoWinningNumbers, 7);
         Assertions.assertThat(lottoNumberCounter.getCount()).isEqualTo(4);
         Assertions.assertThat(lottoNumberCounter.hasBonusNumber()).isEqualTo(true);
     }
@@ -41,9 +41,9 @@ class LottoNumberCounterTest {
     @DisplayName("로또 5등 당첨 시 LottoConstant.FIFTH_PRIZE_KEY 반환 테스트")
     void decideLottoPrize() {
         //given
-        lottoNumberCounter.countLottoNumbers(1);
-        lottoNumberCounter.countLottoNumbers(2);
-        lottoNumberCounter.countLottoNumbers(3);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 1);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 2);
+        lottoNumberCounter.countLottoNumbers(lottoWinningNumbers, 3);
         //when
         Assertions.assertThat(lottoNumberCounter.decideLottoPrize()).isEqualTo("FIFTH_PRIZE");
     }
