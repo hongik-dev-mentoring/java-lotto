60dcda6511c1c186b0937e57255ca74105dd0b17
euichanhwang <pine6971@naver.com>
2023-02-17 20:57:32 +0900

    feat : 조합을 이용해 보너스 볼을 구현한다

--- src/main/java/domain/BonusBall.java
+++ src/main/java/domain/BonusBall.java
@@ -2,38 +2,22 @@ package domain;
 
 public class BonusBall {
 
-	private static final int MIN_LOTTO_NUMBER = 1;
-	private static final int MAX_LOTTO_NUMBER = 45;
+	private final LottoNumber lottoNumber;
+	private final WinningNumbers winningNumbers;
 
-	private final int bonusBallNumber;
-
-	private BonusBall(int bonusBallNumber) {
-		this.bonusBallNumber = bonusBallNumber;
+	public BonusBall(LottoNumber lottoNumber, WinningNumbers winningNumbers) {
+		this.lottoNumber = lottoNumber;
+		this.winningNumbers = winningNumbers;
+		validateDuplicateWithWinningNumbers(lottoNumber, winningNumbers);
 	}
 
-	public static BonusBall createBonusBallInRange(int bonusBallNumber, WinningNumbers winningNumbers) {
-		validateBonusBallNumber(bonusBallNumber);
-		validateDuplicateWithWinningNumber(bonusBallNumber, winningNumbers);
-		return new BonusBall(bonusBallNumber);
-	}
-
-	private static void validateBonusBallNumber(int bonusBallNumber) {
-		if (bonusBallNumber > MAX_LOTTO_NUMBER || bonusBallNumber < MIN_LOTTO_NUMBER) {
-			throw new IllegalArgumentException("[ERROR] 보너스 볼은 1부터 45까지의 숫자여야 합니다.");
+	private void validateDuplicateWithWinningNumbers(LottoNumber lottoNumber, WinningNumbers winningNumbers) {
+		if (winningNumbers.contains(lottoNumber)) {
+			throw new IllegalArgumentException("[ERROR] 당첨 번호와 보너스 볼은 중복이 불가능합니다.");
 		}
 	}
 
-	private static void validateDuplicateWithWinningNumber(int bonusBallNumber, WinningNumbers winningNumbers) {
-		if (isContains(bonusBallNumber, winningNumbers)) {
-			throw new IllegalArgumentException("[ERROR] 보너스 볼은 당첨 번호와 중복이 불가능합니다.");
-		}
-	}
-
-	private static boolean isContains(int bonusBallNumber, WinningNumbers winningNumbers) {
-		return winningNumbers.contains(bonusBallNumber);
-	}
-
-	public int getBonusBallNumber() {
-		return bonusBallNumber;
+	public LottoNumber getLottoNumber() {
+		return lottoNumber;
 	}
 }
