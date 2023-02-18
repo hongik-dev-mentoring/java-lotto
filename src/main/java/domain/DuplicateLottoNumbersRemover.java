e5543398a59f57a21e19f84c0182e6d6f048f549
Junek3 <rlarudrms28@gmail.com>
2023-02-18 23:14:59 +0900

    refactor: DuplicateLottoNumbersRemover의 removeDuplicates 메소드 개선하기

--- src/main/java/domain/DuplicateLottoNumbersRemover.java
+++ src/main/java/domain/DuplicateLottoNumbersRemover.java
@@ -3,16 +3,22 @@ package domain;
 import java.util.List;
 
 public class DuplicateLottoNumbersRemover {
+    private static int duplicateCount = 0;
+
     public static void removeDuplicates(List<LottoNumbers> manualLottoNumbersGroup, List<LottoNumbers> autoLottoNumbersGroup) {
-        int duplicateCount = 0;
         for (LottoNumbers autoLottoNumbers : autoLottoNumbersGroup) {
-            if (manualLottoNumbersGroup.contains(autoLottoNumbers)) {
-                duplicateCount++;
-                autoLottoNumbers.changeNumbers();
-            }
+            changeDuplicateLottoNumbers(manualLottoNumbersGroup, autoLottoNumbers);
         }
         if (duplicateCount != 0) {
+            duplicateCount = 0;
             removeDuplicates(manualLottoNumbersGroup, autoLottoNumbersGroup);
+        }
+    }
+
+    private static void changeDuplicateLottoNumbers(List<LottoNumbers> manualLottoNumbersGroup, LottoNumbers autoLottoNumbers) {
+        if (manualLottoNumbersGroup.contains(autoLottoNumbers)) {
+            duplicateCount++;
+            autoLottoNumbers.changeNumbers();
         }
     }
 }
