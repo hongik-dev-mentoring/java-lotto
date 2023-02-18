1e10cde3d075cce7784557a7ab58a674876877e1
euichanhwang <pine6971@naver.com>
2023-02-18 16:09:33 +0900

    refactor : 당첨 번호의 중복 불가능을 제거한다
    Set 컬렉션을 사용하였기 때문에 중복된 숫자가 일어나면 IllegalArgumentException을 발생시킨다

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -11,19 +11,11 @@ public class WinningNumbers {
 	public WinningNumbers(Set<LottoNumber> winningNumbers) {
 		this.winningNumbers = winningNumbers;
 		validateWinningNumbersLength(winningNumbers);
-		validateDuplicate(winningNumbers);
 	}
 
 	private void validateWinningNumbersLength(Set<LottoNumber> numbers) {
 		if (numbers.size() != LOTTO_NUMBER) {
 			throw new IllegalArgumentException("[ERROR] 당첨 번호는 여섯 개의 숫자여야 합니다.");
-		}
-	}
-
-	private void validateDuplicate(Set<LottoNumber> numbers) {
-		long count = getDistinctLottoNumber(numbers);
-		if (count != numbers.size()) {
-			throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복이 불가능합니다.");
 		}
 	}
 
