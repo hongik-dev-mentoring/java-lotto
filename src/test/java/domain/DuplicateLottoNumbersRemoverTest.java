877b055b2e6c6f1bf1475db2fdd365656ee5999c
Junek3 <rlarudrms28@gmail.com>
2023-02-18 22:22:29 +0900

    feat: 수동 로또와 중복되는 자동 로또는 다시 생성하도록 구현

---
+++ src/test/java/domain/DuplicateLottoNumbersRemoverTest.java
@@ -0,0 +1,47 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DuplicateLottoNumbersRemoverTest {
+    @Test
+    @DisplayName("LottoNumbers 중복 제거 테스트")
+    void removeDuplicatesTest() {
+        // given
+        List<LottoNumbers> manualLottoNumbersGroup = new ArrayList<>();
+        manualLottoNumbersGroup.add(new LottoNumbers(Stream.of(1, 2, 3, 4, 5, 6)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList())
+        ));
+        manualLottoNumbersGroup.add(new LottoNumbers(Stream.of(1, 2, 3, 4, 5, 7)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList())
+        ));
+
+        List<LottoNumbers> autoLottoNumbersGroup = new ArrayList<>();
+        autoLottoNumbersGroup.add(new LottoNumbers(Stream.of(1, 2, 3, 4, 5, 6)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList())
+        ));
+        autoLottoNumbersGroup.add(new LottoNumbers(Stream.of(1, 2, 3, 7, 8, 9)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList())
+        ));
+        // when
+        DuplicateLottoNumbersRemover.removeDuplicates(manualLottoNumbersGroup, autoLottoNumbersGroup);
+        // then
+        LottoNumbers lottoNumbers = new LottoNumbers(Stream.of(1, 2, 3, 4, 5, 6)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList())
+        );
+        assertThat(manualLottoNumbersGroup.get(0).equals(lottoNumbers)).isTrue();
+        assertThat(autoLottoNumbersGroup.get(0).equals(lottoNumbers)).isFalse();
+    }
+}
