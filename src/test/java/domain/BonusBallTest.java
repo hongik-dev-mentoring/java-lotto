60350d407d3c72e755f3488d1f6a27ebc3f14cf7
euichanhwang <pine6971@naver.com>
2023-02-18 14:09:04 +0900

    test : 보너스 볼은 당첨 숫자와 중복이 불가능하다

---
+++ src/test/java/domain/BonusBallTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.DisplayNameGenerator.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class BonusBallTest {
+
+	@ParameterizedTest(name = "보너스볼이 {0}일 때")
+	@ValueSource(ints = {1, 2, 3, 4, 5, 6})
+	void 보너스볼은_당첨_숫자와_중복이_불가능하다(int number) {
+		List<LottoNumber> lottoNumbers = List.of(new LottoNumber(1),new LottoNumber(2),
+			new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(6));
+		WinningNumbers winningNumbers = new WinningNumbers(lottoNumbers);
+
+		assertThatThrownBy(() -> new BonusBall(new LottoNumber(number), winningNumbers))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("[ERROR]");
+	}
+}
