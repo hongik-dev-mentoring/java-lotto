fd7e87fffde5561d5c4c66360db13ef7e653556c
Jinho <dfghcvb11@naver.com>
2023-02-20 03:00:26 +0900

    test : 입력 받은 구입 갯수가 숫자가 아닌 경우 예외를 던지는 테스트 코드 작성

--- src/test/java/util/convertor/PurchaseCountConvertorTest.java
+++ src/test/java/util/convertor/PurchaseCountConvertorTest.java
@@ -1,8 +1,11 @@
 package util.convertor;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PurchaseCountConvertorTest {
 
@@ -11,5 +14,14 @@ class PurchaseCountConvertorTest {
         Integer purchaseCount = PurchaseCountConvertor.convertPurchaseCount("5");
 
         assertThat(purchaseCount).isEqualTo(5);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "ABC"})
+    void 구입_갯수는_숫자여야_한다(String input) {
+
+        assertThatThrownBy(() -> PurchaseCountConvertor.convertPurchaseCount(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 수동 로또 구매 갯수는 숫자여야 합니다. 다시 입력해주세요");
     }
 }
