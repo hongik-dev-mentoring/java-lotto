5dd1329b752fb81b97757b9055902ba1f73780bf
Jinho <dfghcvb11@naver.com>
2023-02-20 02:57:41 +0900

    feat : 구입 갯수를 숫자로 변환하는 기능 구현

---
+++ src/main/java/util/convertor/PurchaseCountConvertor.java
@@ -0,0 +1,8 @@
+package util.convertor;
+
+public class PurchaseCountConvertor {
+
+    public static Integer convertPurchaseCount(String purchaseCountInput) {
+        return Integer.parseInt(purchaseCountInput);
+    }
+}
