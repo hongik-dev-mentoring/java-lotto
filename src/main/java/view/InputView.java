38d982f89cf98225504dc1d8ab4b6ce4c187379c
Jinho <dfghcvb11@naver.com>
2023-02-20 03:44:36 +0900

    feat : InputView에서 수동 로또 구매 갯수와 수동 로또 번호의 입력을 받는 메서드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,8 @@ public class InputView {
 	private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
 	private static final String INPUT_WINNING_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 	private static final String INPUT_BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+	private static final String INPUT_MANUAL_LOTTO_PURCHASE_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+	private static final String INPUT_MANUAL_LOTTO_NUMBERS_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
 	private static final Scanner console = new Scanner(System.in);
 
 	public static String getPurchaseAmount() {
@@ -21,6 +23,16 @@ public class InputView {
 
 	public static String getBonusBallNumber() {
 		System.out.println(INPUT_BONUS_BALL_NUMBER_MESSAGE);
+		return console.nextLine();
+	}
+
+	public static String getManualLottoPurchaseCount() {
+		System.out.println(INPUT_MANUAL_LOTTO_PURCHASE_COUNT_MESSAGE);
+		return console.nextLine();
+	}
+
+	public static String getManualLottoNumbersMessage() {
+		System.out.println(INPUT_MANUAL_LOTTO_NUMBERS_MESSAGE);
 		return console.nextLine();
 	}
 }
