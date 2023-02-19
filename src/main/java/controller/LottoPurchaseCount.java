d1da15c487b55f0d45ce765fce7441cf01fb90dd
Jinho <dfghcvb11@naver.com>
2023-02-20 04:19:12 +0900

    feat : 로또 구매 갯수를 저장하는 객체 LottoPurchaseCount 구현

---
+++ src/main/java/controller/LottoPurchaseCount.java
@@ -0,0 +1,14 @@
+package controller;
+
+public class LottoPurchaseCount {
+
+    final int lottoPurchaseCountCount;
+
+    public LottoPurchaseCount(int lottoPurchaseCountCount) {
+        this.lottoPurchaseCountCount = lottoPurchaseCountCount;
+    }
+
+    public int getLottoPurchaseCountCount() {
+        return lottoPurchaseCountCount;
+    }
+}
