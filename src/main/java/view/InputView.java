70c4a4ed44bed32a42a3ca85cf56abcf675a3964
euichanhwang <pine6971@naver.com>
2023-02-19 10:49:05 +0900

    feat : 수동으로 로또를 구매하는 InputView를  추가한다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,8 @@ public class InputView {
 	private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
 	private static final String INPUT_WINNING_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 	private static final String INPUT_BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+	private static final String MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+	private static final String MANUAL_LOTTO_NUMBER_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
 	private static final Scanner console = new Scanner(System.in);
 
 	public static String getPurchaseAmount() {
@@ -22,5 +24,18 @@ public class InputView {
 	public static String getBonusBallNumber() {
 		System.out.println(INPUT_BONUS_BALL_NUMBER_MESSAGE);
 		return console.nextLine();
+	}
+
+	public static String getManualLottoCount() {
+		System.out.println(MANUAL_LOTTO_COUNT_MESSAGE);
+		return console.nextLine();
+	}
+
+	public static String getManualLottoNumbers() {
+		return console.nextLine();
+	}
+
+	public static void requestManualLotto() {
+		System.out.println(MANUAL_LOTTO_NUMBER_MESSAGE);
 	}
 }
