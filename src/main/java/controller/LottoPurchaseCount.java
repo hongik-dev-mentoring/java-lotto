041b54ea52d13ad902dfacd30bae8fcb53b7cd5c
Jinho <dfghcvb11@naver.com>
2023-02-20 04:32:56 +0900

    feat : 수동 로또 구매 갯수가 구매 로또 갯수를 초과하는 경우 예외를 던지는 기능 구현

--- src/main/java/controller/LottoPurchaseCount.java
+++ src/main/java/controller/LottoPurchaseCount.java
@@ -2,13 +2,22 @@ package controller;
 
 public class LottoPurchaseCount {
 
-    final int lottoPurchaseCountCount;
+    final int lottoPurchaseCount;
 
-    public LottoPurchaseCount(int lottoPurchaseCountCount) {
-        this.lottoPurchaseCountCount = lottoPurchaseCountCount;
+    public LottoPurchaseCount(int lottoPurchaseCount) {
+        this.lottoPurchaseCount = lottoPurchaseCount;
     }
 
-    public int getLottoPurchaseCountCount() {
-        return lottoPurchaseCountCount;
+    public LottoPurchaseCount decreaseCount(int count) {
+        int result = lottoPurchaseCount - count;
+        if (result < 0) {
+            throw new IllegalArgumentException("[ERROR] 수동 로또 구매 갯수는 구매 가능 갯수를 넘어갈 수 없습니다.");
+        }
+
+        return new LottoPurchaseCount(result);
+    }
+
+    public int getLottoPurchaseCount() {
+        return lottoPurchaseCount;
     }
 }
