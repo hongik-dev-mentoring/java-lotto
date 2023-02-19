6326533366c3a267bd077e2ff38520597199503e
Jinho <dfghcvb11@naver.com>
2023-02-20 03:01:47 +0900

    feat : 입력 받은 구입 갯수가 숫자가 아닌 경우 예외를 던지는 기능 구현

--- src/main/java/util/convertor/PurchaseCountConvertor.java
+++ src/main/java/util/convertor/PurchaseCountConvertor.java
@@ -3,6 +3,11 @@ package util.convertor;
 public class PurchaseCountConvertor {
 
     public static Integer convertPurchaseCount(String purchaseCountInput) {
-        return Integer.parseInt(purchaseCountInput);
+        try {
+            Integer purchaseCount = Integer.parseInt(purchaseCountInput);
+            return purchaseCount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 수동 로또 구매 갯수는 숫자여야 합니다. 다시 입력해주세요");
+        }
     }
 }
