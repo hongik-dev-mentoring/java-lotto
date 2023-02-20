bbfe1de6e20741a300d30a8af3112c72412bf8b2
euichanhwang <pine6971@naver.com>
2023-02-20 20:11:11 +0900

    refactor : 로또에 사용할 로또 번호를 미리 만들어 둔 후 재사용한다

--- src/main/java/util/generator/RandomLottoNumberGenerator.java
+++ src/main/java/util/generator/RandomLottoNumberGenerator.java
@@ -1,20 +1,29 @@
 package util.generator;
 
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import domain.LottoNumber;
 
 public class RandomLottoNumberGenerator {
 
+	private static final List<Integer> lottoNumbers;
 	private static final Random random;
+	private static final int MIN_LOTTO_NUMBER = 1;
 	private static final int MAX_LOTTO_NUMBER = 45;
 
 	static {
 		random = new Random();
 		random.setSeed(System.currentTimeMillis());
+		lottoNumbers = IntStream.rangeClosed(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
+			.boxed()
+			.collect(Collectors.toList());
 	}
 
 	public static LottoNumber generate() {
-		return new LottoNumber(random.nextInt(MAX_LOTTO_NUMBER) + 1);
+		int randomIndex = random.nextInt(lottoNumbers.size());
+		return new LottoNumber(lottoNumbers.get(randomIndex));
 	}
 }
