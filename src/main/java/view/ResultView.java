09e6ff442a66431c40b4bda79c8707492e4e319a
LTSGOD <lts0424@naver.com>
2023-02-21 15:10:40 +0900

    feat: 수동, 자동 로또 Input, Output 추가

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -3,13 +3,20 @@ package view;
 import domain.LottoNumbers;
 import domain.LottoPrize;
 
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ResultView {
-    public static void printLottoNumbers(LottoNumbers lottoNumbers) {
+    public static void printLottoNumbers(List<LottoNumbers> lottoNumberGroup) {
+        for (LottoNumbers lottoNumber : lottoNumberGroup) {
+            printLottoNumber(lottoNumber);
+        }
+    }
+
+    private static void printLottoNumber(LottoNumbers lottoNumber) {
         System.out.print("[");
-        String result = lottoNumbers.getLottoNumbers()
+        String result = lottoNumber.getLottoNumbers()
                 .stream()
                 .sorted()
                 .map(String::valueOf)
@@ -17,6 +24,7 @@ public class ResultView {
         System.out.print(result);
         System.out.println("]");
     }
+
 
     public static void printStatistics(Map<LottoPrize, Integer> lottoStatistics) {
         System.out.println("당첨 통계");
@@ -41,11 +49,15 @@ public class ResultView {
         System.out.printf("%.2f", benefit);
     }
 
-    public static void printPurchaseInfo(int purchaseNum) {
-        System.out.println(purchaseNum + "개를 구매했습니다.");
-    }
-
     public static void printInputGuide() {
         System.out.println("지난 주 당첨번호를 입력하세요.");
+    }
+
+    public static void printManualLottoInputGuide() {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+    }
+
+    public static void printPurchaseLottoDetails(int manualLottoNum, int autoLottoNum) {
+        System.out.println("수동으로 " + manualLottoNum + "장, 자동으로 " + autoLottoNum + "개를 구매했습니다.");
     }
 }
